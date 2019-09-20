16b32fab1586f856d42e151c72dd364652ec094f
oguzozgen <oguzozgen@outlook.com>
2019-09-20 11:47:58 +0000

    on progress

--- src/components/introCardComp/IntroCardComp.js
+++ src/components/introCardComp/IntroCardComp.js
@@ -9,7 +9,9 @@ import Button from '@material-ui/core/Button'
 import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
 import RoomIcon from '@material-ui/icons/Room'
-import {Linkedin,Github,Twitter } from 'grommet-icons';
+import {Linkedin,Github,Twitter } from 'grommet-icons'
+import UpworkIcon from '../../assets/upworkIcon24x24Colored.png'
+
 
 import './IntroCardComp.css'
 
@@ -39,26 +41,32 @@ function IntroCardComp(){
                  <Card className={classes.card}>                    
                     <CardContent>
                         <Typography className={classes.headerTypo} gutterBottom variant="h5" component="h2">
-                            <RoomIcon/> 
+                            <RoomIcon/>  Oğuz Özgen
                         </Typography>
                         <Typography className={classes.textTypo} variant="body2" color="textSecondary" component="p">
-                            Hey <br/> Oğuz Özgen is here<br/> I work as a independent developer<br/> 
+                        Trying to do well and be creative <br/>Exploring the missing gear of technology <br/>Learning every-part of my interest
                         </Typography>
+                       
                     </CardContent>                    
                     <CardActions className={classes.cardAction}>                    
                     <Button variant="outlined" size="small" style={{filter:'brightness(1.2)'}}>
                         <Link to="/skillset" >Skill Set</Link>  
                     </Button>
-                    </CardActions>
+                    </CardActions>                   
                     <Divider /> 
-                    <CardActions className={classes.cardAction}>
+                    <CardActions className={classes.cardAction}>                    
+                         <Button>
+                              <a target="_blank" href=" https://www.upwork.com/o/profiles/users/_~015820b9694de8a2d1/" >
+                                    <img src ={UpworkIcon} />
+                              </a>
+                        </Button>  
                          <Button>
                               <a target="_blank" href="https://www.linkedin.com/in/oguzozgen">
                                  <Linkedin/>
                                 </a>
                         </Button>   
                         <Button>                       
-                            <a target="_blank" href="https://github.com/oguzozgen">
+                            <a target="_blank" href="https://github.com/oguzozgen" >
                                 <Github/>    
                             </a>
                         </Button>
@@ -67,6 +75,12 @@ function IntroCardComp(){
                                 <Twitter/>
                             </a>
                         </Button> 
+                    </CardActions>
+                    <Divider />
+                    <CardActions className={classes.cardAction}>  
+                        <Typography className={classes.textTypo} variant="h11" color="textSecondary" >
+                            Independent Developer
+                        </Typography>
                     </CardActions>
                 </Card>    
     );
